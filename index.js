cf88c4db41459156b5603b3c2f77a39f2170f36e
atakannerturk <46032754+atakannerturk@users.noreply.github.com>
2023-05-16 13:12:28 +0300

    Index.js updated

--- index.js
+++ index.js
@@ -14,23 +14,24 @@ dotenv.config({
 });
 app.use("/api", routers);
 
-app.use(function (next) {
-  next(new ErrorHandler(404, "Page not found"));
-});
-app.use((err, req, res) => {
+app.use(function (req, res, next) {
+  next(new ErrorHandler(404, 'Page not found'));
+})
+
+app.use((err, req, res, next) => {
   if (err.constructor === ErrorHandler) {
-    handleError(req, res, err);
-  } else if (err.constructor === NoDataFound) {
-    handleError(new ErrorHandler(400, "Not found"), res);
-  } else if (err.code === "ECONNREFUSED") {
-    handleError(
-      new ErrorHandler(503, `${err.address}:${err.port} refused connection`),
-      res
-    );
-  } else {
-    handleError(new ErrorHandler(500, "Internal Server Error"), res);
+    handleError(req,res,err)
   }
-});
+  else if (err.constructor === NoDataFound) {
+    handleError(new ErrorHandler(400, 'Not found'), res)
+  }
+  else if (err.code === 'ECONNREFUSED') {
+    handleError(new ErrorHandler(503, `${err.address}:${err.port} refused connection`), res)
+  }
+  else {
+    handleError(new ErrorHandler(500,  'Internal Server Error'), res)
+  }
+})
 
 var port = process.env.PORT || "5055";
 
